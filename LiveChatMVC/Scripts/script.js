7d0938d70d73960cb493787a6aa7cbaf959cbb41
Hasan Yesil <hasan.yesil22@outlook.com>
2019-07-14 13:27:53 +0300

    Add project files.

---
+++ LiveChatMVC/Scripts/script.js
@@ -0,0 +1,206 @@
+﻿// Prepairing UI
+(function ($) {
+    $(document).ready(function () {
+        var $chatbox = $('.chatbox'),
+            $chatboxTitle = $('.chatbox__title'),
+            $chatboxTitleClose = $('.chatbox__title__close')
+            
+
+            $chatboxTitle.on('click', function () {
+            $chatbox.toggleClass('chatbox--tray');
+            unReadMessageCount = 0
+            $('#message-alert').css("display", "none")
+        });
+        $chatboxTitleClose.on('click', function (e) {
+            e.stopPropagation();
+            $chatbox.toggleClass('chatbox--tray');
+        })
+    });
+
+    $('#new-message-alert').click(function () {
+        $('#chat_body').animate({ scrollTop: $('#chat_body')[0].scrollHeight }, 400)
+    })
+
+})(jQuery);
+
+// Variable for handling chatHub class.
+var chat = $.connection.chatHub;
+
+var unReadMessageCount = 0
+var unReadMessageCountNow = 0
+// Adding message to ui. Must invoke from hub when someone send message
+chat.client.addNewMessageToPage = function (name, message, creationDate) {
+
+    unReadMessageCount++
+
+    newMessageLeft(message, creationDate, name)
+
+    if ($('#new-message-alert').is(':visible') == false) {
+        $('#chat_body').scrollTop($('#chat_body')[0].scrollHeight)
+    }
+
+    console.log($('#chat_body').scrollTop() - $('#chat_body')[0].scrollHeight)
+
+    if ($('#chat-window').hasClass('chatbox--tray') && unReadMessageCount > 0) {
+        $('#message-alert').css("display", "block")
+        $('#message-count').html(unReadMessageCount)
+    }
+    else {
+        unReadMessageCount = 0
+    }
+
+    if (($('#chat_body').scrollTop() - $('#chat_body')[0].scrollHeight) < 287) {
+        unReadMessageCountNow++
+        $('#unread-message-count').css("display", "block").html(unReadMessageCountNow)
+    }
+}
+
+chat.client.printMessageHistory = function (message, userId) {
+    message.forEach(function (element) {
+
+        if (!(userId == element.u.Id)) {
+            newMessageLeft(element.m.MessageBody, element.m.CreatedAt, element.u.Name)
+        } else {
+            newMessageRight(element.m.MessageBody, element.m.CreatedAt, element.u.Name, false)
+        }
+        $('#chat_body').scrollTop($('#chat_body')[0].scrollHeight);
+    })
+}
+
+chat.client.userLeft = function (userName) {
+    userLeftAlert(userName)
+}
+
+// Add new message to callerclient
+chat.client.addNewMessageToMe = function (name, message, creationDate) {
+    newMessageRight(message, creationDate, name, true)
+}
+
+//Safe New Message Functions
+function newMessageLeft(MessageBody, CreatedAt, Name) {
+   
+    
+    var stringHTML = ('<div class="chatbox__body__message chatbox__body__message--left">'
+        + '<div class="clearfix"></div>'
+        + '<div class="ul_section_full">'
+        + '<ul class="ul_msg">'
+        + '<li style="font-size:10px"  class="pb-2 text-capitalize d-flex justify-content-end"><i>' + Name + '</i></li>'
+        + '<li class="message">' + MessageBody + '</li>'
+        + '<li class="timing"><span class="mr-2">' + formatDate(CreatedAt) + '</span><i class="fa fa-clock"></li>'
+        + '</ul>'
+        + '<div class="clearfix"></div>'
+        + '</div>'
+        + '</div>')
+
+    var html = $.parseHTML(stringHTML)
+    var chatBody = $('#chat_body')
+
+    chatBody.append(html)
+}
+
+function newMessageRight(MessageBody, CreatedAt, Name, isMe) {
+
+    var stringHTML = ('<div class="chatbox__body__message chatbox__body__message--right">'
+        + '<div class="clearfix"></div>'
+        + '<div class="ul_section_full">'
+        + '<ul class="ul_msg">'
+        + '<li style="font-size:10px"  class="pb-2 text-capitalize d-flex justify-content-end"><i>' + Name + '</i></li>'
+        + '<li class="message">' + MessageBody + '</li>'
+        + '<li class="timing"><span class="mr-2">' + formatDate(CreatedAt) + '</span><i class="fa fa-clock"></li>'
+        + '</ul>'
+        + '<div class="clearfix"></div>'
+        + '</div>'
+        + '</div>')
+
+    var html = $.parseHTML(stringHTML)
+    var chatBody = $('#chat_body')
+
+    if (isMe) {
+        chatBody.append(html).animate({ scrollTop: $('#chat_body')[0].scrollHeight }, 400)
+    }
+    else {
+        chatBody.append(html)
+    }
+}
+
+function userLeftAlert(userName) {
+    var stringHTML = ('<div class="chatbox__body__message chatbox__body__message--left">'
+        + '<div class="clearfix"></div>'
+        + '<div class="ul_section_full_red">'
+        + '<ul class="ul_msg d-flex justify-content-between">'
+        + '<li class="message" style="font-size:10px">' + userName + " ayrıldı." + '</li>'
+        + '<li class="timing"><span class="mr-2">' + new Date().toLocaleTimeString() + '</span><i class="fa fa-clock"></li>'
+        + '</ul>'
+        + '<div class="clearfix"></div>'
+        + '</div>'
+        + '</div>')
+
+    var html = $.parseHTML(stringHTML)
+    $('#chat_body').append(html)
+}
+
+
+function formatDate(date) {
+
+    var newDate = new Date(date)
+    var messageDate = moment(date, "YYYY-MM-DD");
+    var current = moment().startOf('day');
+    var diff = moment.duration(messageDate.diff(current)).asDays();
+
+    var formattedDate = diff < 0 ? moment(newDate).fromNow() : moment(newDate).format("HH:mm")
+
+    return formattedDate
+}
+
+// TODO: Show count of online users in chat.
+chat.client.updateUserCount = function (count) {
+    if (count < 2) {
+        $('#title').addClass('gray_title')
+        var html = $.parseHTML('<a href="#">Chat ( <i class="fas fa-user-tie pr-2"></i>' + (count - 1) + ' Online)</a>')
+        $('#title').html(html)
+    }
+    else {
+        $('#title').removeClass('gray_title')
+        var html = $.parseHTML('<a href="#">Chat ( <i class="fas fa-user-tie pr-2"></i>' + (count - 1) + ' Online)</a>')
+        $('#title').html(html)
+    }
+}
+// When hub started succesfully or fail.
+$.connection.hub.start().done(function () {
+
+    var token = prompt('Sender Token : ', '');
+    chat.server.connect(token);
+
+    $('#btn-chat').on('click', function () {
+
+        var value = $('#btn-input').val()
+        if (value == '' || value == null) {
+            console.log('Boş mesaj atılamaz!')
+        }
+        else {
+            chat.server.send(value);
+            $('#btn-input').val('');
+            $('#btn-input').focus();
+        }
+    });
+    var $chatBody = $('#chat_body')
+    $chatBody.scroll(function () {
+        if (($chatBody[0].scrollHeight) - $chatBody.scrollTop() > 287) {
+            $('#new-message-alert').fadeIn('slow')
+        }
+        else {
+            $('#new-message-alert').fadeOut('fast')
+            $('#unread-message-count').css("display", "none")
+            unReadMessageCountNow = 0
+        }
+    }) 
+}).fail(function () {
+    console.log("Hub starting fail")
+});
+
+//Enter Trigger
+$('#btn-input').keyup(function (e) {
+    if (e.keyCode == 13) {
+        $('#btn-chat').click()
+    }
+})
---
+++ LiveChatMVC/signalr/hubs/ChatHub.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiveChatMVC.Models;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+using LiveChatMVC.DummyApi;
+using User = LiveChatMVC.Models.User;
+using System.Collections.Concurrent;
+using System.Data.Entity;
+
+namespace LiveChatMVC.signalr.hubs
+{
+    // PS: When client call function from hub, server creates ChatHub object instance.
+    // TODO: Need exception handling. 
+    public class ChatHub : Hub
+    {
+        // This dictionary contains data about which user connected to hub from which client id.
+        // key = connectionId, value = Users object. We use ConcurrentDictionary because of thread safety.
+        // We have to update this dictionary when user connected or when user disconnected or when user 
+        // update her/his connection id like after refleshing the page
+        private static ConcurrentDictionary<string, Users> SignalRUsers = new ConcurrentDictionary<string, Users>();
+
+        private string jsonStringUser; // Json string from api contains user information.
+        private JObject jsonUser;   // Json object for user informations.
+
+        static readonly Gldnhrn api = new Gldnhrn(); // Dummy api looks like goldenhorn api
+        static readonly ChatContext db = new ChatContext(); // Database
+
+        // PS: You have to override onConnect function normally but its example.
+        
+        public void Connect(string senderToken)
+        {
+            // Fetching jsonstring contains user information from api via token
+            jsonStringUser = api.requestUser(senderToken);
+
+            // Is user exist or not 
+            if (jsonStringUser == null)
+                return;
+
+            // Convert JSON string to object
+            jsonUser = JObject.Parse(jsonStringUser);
+
+            // Initialiazing variables
+            int userId = int.Parse(jsonUser["userId"].ToString());
+            string userName = jsonUser["userName"].ToString();
+            string chatId = api.ticketId();
+            string userRole = jsonUser["role"].ToString();
+
+            // We cant store object in hub because when client call function from hub, server create hub instance and process the function. After that
+            // server remove hub intance. So out assigned variables will remove with hub instance. You cannot access any data again.(except static variables.)
+            // So store it in CallerState if u want to access variables from outside the functions
+            // PS: CallerState is reachable on both client and server side.
+            Clients.CallerState.userName = userName;
+            Clients.CallerState.userId = userId;
+            Clients.CallerState.chatId = chatId; 
+
+
+            // Search user on database.
+            var users = db.Users.Where(i => i.Id == userId).ToList();
+
+            // If user doesnt exist create new user and insert it to database.
+            if (users.Count < 1)
+            {
+                insertNewUser(userId, userName);
+            }
+
+            // Search for chatroom on database
+            // PS: tickedId(assing to chatId on above) will come from GoldenHorn api so we use static chatId for this example like its come from goldenhorn api.
+            // If you use ticketId as a signalr group name like us its gonna be easier.
+            var chats = db.Chats.Where(i => i.Id == chatId).ToList();
+
+            // If chat doesnt exist create new chat and add to database.
+            if (chats.Count < 1)
+            {
+                insertNewChat(chatId);
+            }
+
+            // Insert chat and user relation to database.
+            //insertChatUserRelation(chatId, userId);
+
+            // If user exist in chat but his/her connection id changed.( if user reload the page while in chat etc.)
+            if (SignalRUsers.Values.Any(user => (user.GroupNames.Contains(chatId) && user.UserName == userName)))
+            {
+                // Update users connection id.
+                SignalRUsers.TryRemove(SignalRUsers.FirstOrDefault(i => i.Value.UserName == userName).Key, out Users updatedUser);
+                updatedUser.ConnectionId = Context.ConnectionId;
+                SignalRUsers.TryAdd(Context.ConnectionId, updatedUser);
+
+                // Add to chat and notify chat 
+                _ = joinGroup(chatId);
+                Clients.Group(chatId).updateUserCount(SignalRUsers.Where(i => i.Value.GroupNames.Contains(chatId)).Count());
+            }
+            // If user not in chat room
+            else
+            {
+                SignalRUsers.TryAdd(Context.ConnectionId, new Users { ConnectionId = Context.ConnectionId, UserName = userName, GroupNames = new List<string>() { chatId } });
+                _ = joinGroup(chatId);
+                Clients.Group(chatId).updateUserCount(SignalRUsers.Count());
+            }
+
+            // Fetch old messages where sended in chat.
+            _ = fetchChatHistory(chatId, userId);
+        }
+
+        // This function works when client wants to send message to group
+        public void Send(string message)
+        {
+            // Initialiaze variables
+            string userName = Clients.CallerState.userName;
+            string chatId = Clients.CallerState.chatId;
+
+            // Update Message table on database.(Insert message to message table)
+
+            var msg = insertNewMessage(message, chatId);
+
+            // Send message to group.
+            Clients.OthersInGroup(chatId).addNewMessageToPage(userName, message, msg.CreatedAt);
+
+            // Show sended message on caller clients iu.
+            Clients.Caller.addNewMessageToMe(userName, message, msg.CreatedAt);
+        }
+
+        private void insertNewUser(int userId, string userName)
+        {
+            var user = new User
+            {
+                Id = userId,
+                Name = userName,
+            };
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        private void insertNewChat(string chatId)
+        {
+            var chat = new Chat
+            {
+                Id = chatId,
+            };
+            db.Chats.Add(chat);
+            db.SaveChanges();
+        }
+
+        private void insertChatUserRelation(string chatId, int userId)
+        {
+            var userChat = new UserChat
+            {
+                ChatId = chatId,
+                UserId = userId
+            };
+            db.UserChats.Add(userChat);
+            db.SaveChanges();
+        }
+
+        private Message insertNewMessage(string message, string chatId)
+        {
+            var msg = new Message
+            {
+                MessageBody = message,
+                ChatId = chatId,
+                UserId = Convert.ToInt32(Clients.CallerState.userId),
+                CreatedAt = DateTime.Now
+            };
+
+            db.Messages.Add(msg);
+            db.SaveChanges();
+
+            return msg;
+        }
+
+        //Add to group.
+        private async Task joinGroup(string chatId)
+        {
+            await Groups.Add(Context.ConnectionId, chatId);
+
+            // Update user count on group.
+            await Clients.Group(chatId).updateUserCount(SignalRUsers.Values.Where(user => user.GroupNames.Contains(chatId)).Count());
+        }
+
+        private async Task fetchChatHistory(string chatId, int userId)
+        {
+            var messagesAndUsers = await (from u in db.Users
+                                          join m in db.Messages on u.Id equals m.UserId
+                                          select new { u, m }).Where(i => i.m.ChatId == chatId).ToListAsync();
+
+            // If chat has not any messages, return.
+            if (messagesAndUsers.Count < 1)
+                return;
+
+            // Send message histroy as an array to client.
+            await Clients.Caller.printMessageHistory(messagesAndUsers, userId); ;
+        }
+
+        // This function call after 6 second timeout (for more information https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/handling-connection-lifetime-events).
+        // So when user reload the page it takes 1 second and users client id will changed.
+        // We have to handle this on connect function if we still have connection on list we have to re assign new Connection id
+        // We dont need to remove client from group. Signalr makes it for us. And it's recommended to dont remove client from group manually
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string groupName = api.ticketId();
+
+            // If user reload the page and connect function works again, user must removed from list and users new connection id updated. So if user reload the page
+            // and login in 6 seconds then connect function will remove expired user before the OnDisconnected function. We can't removed user again.
+            // Check for user removed on OnDisconnect function.
+            bool isRemoved = SignalRUsers.TryRemove(Context.ConnectionId, out Users disconnectedUser);
+
+            // if isRemoved = false then it means user already updated before OnDisconnect function call. 
+            // (when we reflesh the page client call connect function again and we update users conenction id. That means dictionary has no longer )
+            if (isRemoved)
+            {
+                Clients.OthersInGroup(groupName).userLeft(disconnectedUser.UserName);
+                Clients.OthersInGroup(groupName).updateUserCount(SignalRUsers.Where(i => i.Value.GroupNames.Contains(groupName)).Count());
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+
+    }
+}
